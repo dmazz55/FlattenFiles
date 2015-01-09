eee7656ca05ac035f00bfa067b7cccdf61000cfe
dmazz55 <dmazz55@sbcglobal.net>
2015-01-08 19:27:56 -0500

    added command prompt

--- index.js
+++ index.js
@@ -1,9 +1,70 @@
-var fs = require('fs');
-var async = require('async');
-var util = require('util');
+var fs = require('fs')
+    ,async = require('async')
+    ,util = require('util')
+    ,prompt = require('prompt')
+
 require('string.prototype.endswith');
 
-var startDir = 'C:\\Users\\dmazzeo\\Desktop\\test';
+
+var Print = function(){
+  this.magenta = function(text){
+    console.log(clc.magentaBright(text));
+  };
+  this.white = function(text){
+    console.log(clc.whiteBright(text));
+  };
+  this.cyan = function(text){
+    console.log(clc.cyanBright(text));
+  };
+  this.red = function(text){
+    console.log(clc.redBright(text));
+  };
+  this.yellow = function(text){
+    console.log(clc.yellowBright(text));
+  };
+  this.green = function(text){
+    console.log(clc.greenBright(text));
+  };
+  this.emptyLine = function(){
+    console.log('');
+  };
+}
+
+var p = new Print();
+var startDir;
+
+function main(){
+  p.emptyLine();
+  p.magenta('-----------------------------------');
+  p.magenta('         FlattenFiles v1.0')
+  p.magenta('-----------------------------------'); 
+  p.cyan('       The File Flattener! ... duh');
+  p.emptyLine();
+  p.yellow('Enter folder to flatten or \'exit\' to Quit');
+  promptForInput();
+}
+
+function promptForInput(){
+  prompt.start();
+  prompt.get(['input'], function(err, result){
+    if (!checkResult(result)) return;
+    startDir = result.input;
+    startFlattening();
+  });
+}
+
+var startFlattening = function(){
+  getFiles(startDir, function(err, results) {
+    if (err) throw err;
+    async.each(results,function(file, next){
+      console.log(getNewFilePath(file.fileName));
+      fs.renameSync(file.fullDir,getNewFilePath(file.fileName));
+    },function(err){
+      if (err) p.red(err);
+      p.green('Done!');
+    })
+  });
+}
 
 var getFiles = function(dir, done) {
   var results = [];
@@ -27,14 +88,6 @@ var getFiles = function(dir, done) {
     });
   });
 };
-
-getFiles(startDir, function(err, results) {
-  if (err) throw err;
-  async.each(results,function(file, next){
-    console.log(getNewFilePath(file.fileName));
-  	//fs.renameSync(file.fullDir,startDir + file.fileName);
-  })
-});
 
 function getNewFilePath(fileName){
   return startDir.endsWith('\\') ? startDir + fileName : startDir + '\\' + fileName;
